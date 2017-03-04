a2692284e66485fb4439ad89564b560e84020439
Surajit <suraj.roland@gmail.com>
2017-03-04 23:47:16 +0100

    delta commit

---
+++ _site/README.md
@@ -0,0 +1,3 @@
+# Shuro weds Teju
+
+The wedding website of Shuro and Teju [http://www.tejuandshuro.com](http://www.tejuandshuro.com)
---
+++ _site/js/drawmap.js
@@ -0,0 +1,149 @@
+
+  // Drawing googleMaps in the contact background
+
+function drawGoogleMap() {
+
+    var map;
+
+    var style = [
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#5e5e5e"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+]
+
+    var office = {lat: 47.6600237, lng: 15.4793332};
+    var office_map = {lat: 47.6600237, lng: 9.4793332};
+
+    var options = {
+        zoom: 5,
+        zoomControl: false,
+        scaleWheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+        disableDoubleClickZoom: true,
+        center:  office,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+    };
+
+    map = new google.maps.Map($('#map')[0], options);
+    map.setOptions({
+        styles: style,
+        scrollwheel: false
+    });
+
+    var marker = new google.maps.Marker({
+    position: office_map,
+    map: map,
+    title: 'I live here'
+    });
+
+    google.maps.event.trigger(map, 'resize');
+
+}
+
+google.maps.event.addDomListener(window, 'load', drawGoogleMap);
